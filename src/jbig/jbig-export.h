463db3f7a6bb6146f9577470991a3b94062055f5
Dmitry Baryshev <dmitrymq@gmail.com>
2020-09-18 22:34:50 +0300

    Fix JBIG exporting macros

--- src/jbig/jbig-export.h
+++ src/jbig/jbig-export.h
@@ -1,7 +1,7 @@
 #ifndef JBIG_EXPORT_H
 #define JBIG_EXPORT_H
 
-#ifdef _WIN32 || defined __CYGWIN__
+#if defined _WIN32 || defined __CYGWIN__
     #ifdef JBIG_BUILD
         #define JBIG_EXPORT __declspec(dllexport)
     #else
@@ -10,8 +10,8 @@
 
     #define JBIG_HIDDEN
 #else
-    #define SAIL_EXPORT __attribute__((visibility("default")))
-    #define SAIL_HIDDEN __attribute__((visibility("hidden")))
+    #define JBIG_EXPORT __attribute__((visibility("default")))
+    #define JBIG_HIDDEN __attribute__((visibility("hidden")))
 #endif
 
 #endif
